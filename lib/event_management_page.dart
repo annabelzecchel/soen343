73590ccc300948687ab19a09fabd8187bdde0f22
irizhukova <ira.zhu.2003@gmail.com>
2025-03-14 22:55:37 -0400

    event multi page dashboard

--- lib/event_management_page.dart
+++ lib/event_management_page.dart
@@ -3,29 +3,93 @@ import 'package:flutter/material.dart';
 class EventManagementPage extends StatefulWidget {
   final String title;
   const EventManagementPage({super.key, required this.title});
- 
-    @override
-    State<EventManagementPage> createState() => _EventManagementState();
-  }
-  
-  class _EventManagementState extends State<EventManagementPage> {
-    @override
-    Widget build(BuildContext context) {
-      return Scaffold(
-        appBar: AppBar(
-          title: const Text('Event Management for Organizers'),
-        ),
-        body: const Center(
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: <Widget>[
-              Text(
-                'Event Management',
-                style: TextStyle(fontSize: 24),
+
+  @override
+  State<EventManagementPage> createState() => _EventManagementState();
+}
+
+class _EventManagementState extends State<EventManagementPage> {
+  final List<Widget> _screens = [
+    //Content Event page tab
+    Container(
+        color: Colors.red,
+        alignment: Alignment.center,
+        child: const Text(
+          'Events',
+          style: TextStyle(fontSize: 40),
+        )),
+    Container(
+        color: Colors.blue,
+        alignment: Alignment.center,
+        child: const Text(
+          'Profile',
+          style: TextStyle(fontSize: 40),
+        )),
+    Container(
+        color: Colors.green,
+        alignment: Alignment.center,
+        child: const Text(
+          'Settings',
+          style: TextStyle(fontSize: 40),
+        )),
+    Container(
+        color: Colors.yellow,
+        alignment: Alignment.center,
+        child: const Text(
+          'About',
+          style: TextStyle(fontSize: 40),
+        ))
+  ];
+  int _selectedIndex = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+        backgroundColor: Colors.deepPurple,
+      ),
+      body: Row(
+        children: [
+          Container(
+            width: 200,
+            decoration: const BoxDecoration(
+              border: Border(right: BorderSide(color: Colors.grey, width: 1)), // Border for separation
+            ),
+            child: NavigationRail(
+              onDestinationSelected: (int index) {
+                setState(() {
+                  _selectedIndex = index;
+                });
+              },
+              selectedIndex: _selectedIndex,
+              destinations: const [
+                NavigationRailDestination(
+                  icon: Icon(Icons.event),
+                  label: Text('Events'),
+                ),
+                NavigationRailDestination(
+                  icon: Icon(Icons.person),
+                  label: Text('Profile'),
+                ),
+                NavigationRailDestination(
+                  icon: Icon(Icons.settings),
+                  label: Text('Settings'),
+                ),
+                NavigationRailDestination(
+                  icon: Icon(Icons.info),
+                  label: Text('About'),
+                ),
+              ],
+              labelType: NavigationRailLabelType.all,
+              selectedLabelTextStyle: const TextStyle(
+                color: Colors.blue,
               ),
-            ],
+            ),
           ),
-        ),
-      );
-    }
+          Expanded(child: _screens[_selectedIndex]),
+        ],
+      ),
+    );
   }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -4,6 +4,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import "package:cloud_firestore/cloud_firestore.dart";
 import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:soen343/event_management_page.dart';
 
 void main() async {
   await dotenv.load();
@@ -40,7 +41,8 @@ class MyApp extends StatelessWidget {
           colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
           useMaterial3: true,
         ),
-        home: const MyHomePage(title: 'Flutter Demo Home Page'),
+        home: const MyHomePage(title: 'Home Page'),
+        debugShowCheckedModeBanner: false,
       ),
     );
   }
@@ -55,59 +57,9 @@ class MyHomePage extends StatefulWidget {
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
-class EventManagementPage extends StatelessWidget {
-  const EventManagementPage({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text('Event Management Page')),
-      body: Center(
-        child: ElevatedButton(
-          onPressed: () {
-            Navigator.pop(context); // Go back to Home Page
-          },
-          child: const Text('Go Back'),
-        ),
-      ),
-    );
-  }
-}
-
 class _MyHomePageState extends State<MyHomePage> {
   final nameController = TextEditingController();
   final typeController = TextEditingController();
-  int _counter = 0;
-
-  final List<Widget> _screens = [
-    //Content Event page tab
-    Container(
-        color: Colors.red,
-        child: const Text(
-          'Events',
-          style: TextStyle(fontSize: 40),
-        )),
-    Container(
-        color: Colors.blue,
-        child: const Text(
-          'Profile',
-          style: TextStyle(fontSize: 40),
-        )),
-    Container(
-        color: Colors.green,
-        child: const Text(
-          'Settings',
-          style: TextStyle(fontSize: 40),
-        )),
-    Container(
-        color: Colors.yellow,
-        child: const Text(
-          'About',
-          style: TextStyle(fontSize: 40),
-        ))
-  ];
-
-  int _selectedIndex = 0;
 
   @override
   Widget build(BuildContext context) {
@@ -121,84 +73,45 @@ class _MyHomePageState extends State<MyHomePage> {
             onPressed: () {
               Navigator.push(
                 context,
-                MaterialPageRoute(builder: (context) => const EventManagementPage()),
+                MaterialPageRoute(
+                    builder: (context) => const EventManagementPage(title: 'Event Management for Organizers')),
               );
             },
           ),
         ],
       ),
-      body: Row(
-        children: [
-          NavigationRail(
-            onDestinationSelected: (int index) {
-              setState(() {
-                _selectedIndex = index;
-              });
-            },
-            selectedIndex: _selectedIndex,
-            destinations: const [
-              NavigationRailDestination(
-                icon: Icon(Icons.event),
-                label: Text('Events'),
-              ),
-              NavigationRailDestination(
-                icon: Icon(Icons.person),
-                label: Text('Profile'),
-              ),
-              NavigationRailDestination(
-                icon: Icon(Icons.settings),
-                label: Text('Settings'),
-              ),
-              NavigationRailDestination(
-                icon: Icon(Icons.info),
-                label: Text('About'),
-              ),
-            ],
-            selectedLabelTextStyle: const TextStyle(
-              color: Colors.black,
-            ),
-          ),
-          Expanded(
-            child: Column(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                _screens[_selectedIndex], // Display the selected section
-              ],
-            ),
-          ),
-          // TextFormField(
-          //   controller: nameController,
-          //   decoration: const InputDecoration(
-          //     hintText: 'Name',
-          //   ),
-          // ),
-          // TextFormField(
-          //   controller: typeController,
-          //   decoration: const InputDecoration(
-          //     hintText: 'Type',
-          //   ),
-          // ),
-          // ElevatedButton(
-          //   onPressed: () {
-          //     CollectionReference colRef =
-          //         FirebaseFirestore.instance.collection("events");
-          //     colRef.add({
-          //       "name": nameController.text,
-          //       "type": typeController.text,
-          //       "id": colRef.doc().id,
-          //     });
-          //   },
-          //   child: const Text("Add Event"),
-          // ),
-          // const Text(
-          //   'You have pushed the button this many times:',
-          // ),
-          // Text(
-          //   '$_counter',
-          //   style: Theme.of(context).textTheme.headlineMedium,
-          // ),
-        ],
-      ),
+
+      // TextFormField(
+      //   controller: nameController,
+      //   decoration: const InputDecoration(
+      //     hintText: 'Name',
+      //   ),
+      // ),
+      // TextFormField(
+      //   controller: typeController,
+      //   decoration: const InputDecoration(
+      //     hintText: 'Type',
+      //   ),
+      // ),
+      // ElevatedButton(
+      //   onPressed: () {
+      //     CollectionReference colRef =
+      //         FirebaseFirestore.instance.collection("events");
+      //     colRef.add({
+      //       "name": nameController.text,
+      //       "type": typeController.text,
+      //       "id": colRef.doc().id,
+      //     });
+      //   },
+      //   child: const Text("Add Event"),
+      // ),
+      // const Text(
+      //   'You have pushed the button this many times:',
+      // ),
+      // Text(
+      //   '$_counter',
+      //   style: Theme.of(context).textTheme.headlineMedium,
+      // ),
     );
   }
 }
